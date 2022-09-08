83159f27e634cf38eee21e9f458d93adbfa0610d
jiangwenping <jiangwenping@liepin.com>
2022-09-08 20:26:39 +0800

    概览修改

--- vue-element-admin/src/views/dashboard/admin/index.vue
+++ vue-element-admin/src/views/dashboard/admin/index.vue
@@ -89,23 +89,6 @@
         </el-table>
       </el-tab-pane>
     </el-tabs>
-    <el-row :gutter="32">
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart/>
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart/>
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart/>
-        </div>
-      </el-col>
-    </el-row>
 
     <el-row :gutter="8">
       <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
