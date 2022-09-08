019c99c8a83279581a44e106dc4a417d00cfb248
jiangwenping <jiangwenping@liepin.com>
2022-09-08 17:02:13 +0800

    菜单修改

--- vue-element-admin/src/router/index.js
+++ vue-element-admin/src/router/index.js
@@ -6,11 +6,6 @@ Vue.use(Router)
 /* Layout */
 import Layout from '@/layout'
 
-/* Router Modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -40,17 +35,6 @@ import nestedRouter from './modules/nested'
  */
 export const constantRoutes = [
     {
-        path: '/redirect',
-        component: Layout,
-        hidden: true,
-        children: [
-            {
-                path: '/redirect/:path(.*)',
-                component: () => import('@/views/redirect/index')
-            }
-        ]
-    },
-    {
         path: '/login',
         component: () => import('@/views/login/index'),
         hidden: true
@@ -79,49 +63,10 @@ export const constantRoutes = [
                 path: 'dashboard',
                 component: () => import('@/views/dashboard/index'),
                 name: 'Dashboard',
-                meta: {title: 'Dashboard', icon: 'dashboard', affix: true}
+                meta: {title: '首页', icon: 'dashboard', affix: true}
             }
         ]
     },
-    {
-        path: '/documentation',
-        component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/documentation/index'),
-                name: 'Documentation',
-                meta: {title: 'Documentation', icon: 'documentation', affix: true}
-            }
-        ]
-    },
-    {
-        path: '/guide',
-        component: Layout,
-        redirect: '/guide/index',
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/guide/index'),
-                name: 'Guide',
-                meta: {title: 'Guide', icon: 'guide', noCache: true}
-            }
-        ]
-    },
-    {
-        path: '/profile',
-        component: Layout,
-        redirect: '/profile/index',
-        hidden: true,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/profile/index'),
-                name: 'Profile',
-                meta: {title: 'Profile', icon: 'user', noCache: true}
-            }
-        ]
-    }
 ]
 
 /**
@@ -130,132 +75,66 @@ export const constantRoutes = [
  */
 export const asyncRoutes = [
     {
-        path: '/permission',
-        component: Layout,
-        redirect: '/permission/page',
-        alwaysShow: true, // will always show the root menu
-        name: 'Permission',
-        meta: {
-            title: 'Permission',
-            icon: 'lock',
-            roles: ['admin', 'editor'] // you can set roles in root nav
-        },
-        children: [
-            {
-                path: 'page',
-                component: () => import('@/views/permission/page'),
-                name: 'PagePermission',
-                meta: {
-                    title: 'Page Permission',
-                    roles: ['admin'] // or you can only set roles in sub nav
-                }
-            },
-            {
-                path: 'directive',
-                component: () => import('@/views/permission/directive'),
-                name: 'DirectivePermission',
-                meta: {
-                    title: 'Directive Permission'
-                    // if do not set roles, means: this page does not require permission
-                }
-            },
-            {
-                path: 'role',
-                component: () => import('@/views/permission/role'),
-                name: 'RolePermission',
-                meta: {
-                    title: 'Role Permission',
-                    roles: ['admin']
-                }
-            }
-        ]
-    },
-
-    {
-        path: '/icon',
-        component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/icons/index'),
-                name: 'Icons',
-                meta: {title: 'Icons', icon: 'icon', noCache: true}
-            }
-        ]
-    },
-
-    /** when your routing map is too long, you can split it into small modules **/
-    componentsRouter,
-    chartsRouter,
-    nestedRouter,
-    tableRouter,
-
-    {
         path: '/example',
         component: Layout,
-        redirect: '/example/list',
         name: 'Example',
         meta: {
-            title: 'Example',
-            icon: 'el-icon-s-help'
+            title: '账号运营',
+            icon: 'el-icon-data-line'
         },
         children: [
             {
                 path: 'create',
                 component: () => import('@/views/example/create'),
                 name: 'CreateArticle',
-                meta: {title: 'Create Article', icon: 'edit'}
+                meta: {title: '账号管理', icon: 'el-icon-monitor'}
             },
             {
-                path: 'edit/:id(\\d+)',
+                path: 'edit',
                 component: () => import('@/views/example/edit'),
                 name: 'EditArticle',
-                meta: {title: 'Edit Article', noCache: true, activeMenu: '/example/list'},
-                hidden: true
+                meta: {title: '智能发布', icon: 'el-icon-s-promotion'},
             },
             {
                 path: 'list',
                 component: () => import('@/views/example/list'),
                 name: 'ArticleList',
-                meta: {title: 'Article List', icon: 'list'}
+                meta: {title: '视频发布', icon: 'el-icon-video-camera'}
             }
         ]
     },
-
-    {
-        path: '/tab',
-        component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/tab/index'),
-                name: 'Tab',
-                meta: {title: 'Tab', icon: 'tab'}
-            }
-        ]
-    },
-
     {
         path: '/error',
         component: Layout,
-        redirect: 'noRedirect',
         name: 'ErrorPages',
         meta: {
-            title: 'Error Pages',
-            icon: '404'
+            title: '视频创作',
+            icon: 'el-icon-s-check'
         },
         children: [
             {
                 path: '401',
                 component: () => import('@/views/error-page/401'),
                 name: 'Page401',
-                meta: {title: '401', noCache: true}
+                meta: {title: '智能创作', icon: 'el-icon-s-platform'}
             },
             {
-                path: '404',
+                path: '4041',
                 component: () => import('@/views/error-page/404'),
                 name: 'Page404',
-                meta: {title: '404', noCache: true}
+                meta: {title: 'AI智能创作', icon: 'el-icon-magic-stick'}
+            },
+            {
+                path: '4042',
+                component: () => import('@/views/error-page/404'),
+                name: 'Page404',
+                meta: {title: '视频模版', icon: 'el-icon-video-play'}
+            },
+            {
+                path: '4043',
+                component: () => import('@/views/error-page/404'),
+                name: 'Page404',
+                meta: {title: '直播剪辑', icon: 'el-icon-scissors'}
             }
         ]
     },
@@ -263,127 +142,73 @@ export const asyncRoutes = [
     {
         path: '/error-log',
         component: Layout,
+        name: 'ErrorPages',
+        meta: {
+            title: '智能管理',
+            icon: 'el-icon-goblet-square'
+        },
         children: [
             {
                 path: 'log',
                 component: () => import('@/views/error-log/index'),
-                name: 'ErrorLog',
-                meta: {title: 'Error Log', icon: 'bug'}
+                name: 'ErrorLog1',
+                meta: {title: '视频管理', icon: 'el-icon-camera'}
+            },
+            {
+                path: 'log1',
+                component: () => import('@/views/error-log/index'),
+                name: 'ErrorLog2',
+                meta: {title: '评论管理', icon: 'el-icon-chat-dot-square'}
+            },
+            {
+                path: 'log2',
+                component: () => import('@/views/error-log/index'),
+                name: 'ErrorLog3',
+                meta: {title: '私信管理', icon: 'el-icon-chat-square'}
+            },
+            {
+                path: 'log3',
+                component: () => import('@/views/error-log/index'),
+                name: 'ErrorLog4',
+                meta: {title: '线索管理', icon: 'el-icon-collection'}
             }
         ]
     },
-
     {
         path: '/excel',
         component: Layout,
-        redirect: '/excel/export-excel',
         name: 'Excel',
         meta: {
-            title: 'Excel',
-            icon: 'excel'
+            title: '运营管理',
+            icon: 'el-icon-s-marketing'
         },
         children: [
             {
                 path: 'export-excel',
                 component: () => import('@/views/excel/export-excel'),
                 name: 'ExportExcel',
-                meta: {title: 'Export Excel'}
+                meta: {title: '智能触客', icon: 'el-icon-headset'}
             },
             {
                 path: 'export-selected-excel',
                 component: () => import('@/views/excel/select-excel'),
                 name: 'SelectExcel',
-                meta: {title: 'Export Selected'}
+                meta: {title: 'SEO热词', icon: 'el-icon-search'}
             },
             {
                 path: 'export-merge-header',
                 component: () => import('@/views/excel/merge-header'),
                 name: 'MergeHeader',
-                meta: {title: 'Merge Header'}
+                meta: {title: '素材管理', icon: 'el-icon-picture'}
             },
             {
                 path: 'upload-excel',
                 component: () => import('@/views/excel/upload-excel'),
                 name: 'UploadExcel',
-                meta: {title: 'Upload Excel'}
+                meta: {title: 'WebHook', icon: 'el-icon-apple'}
             }
         ]
     },
-
-    {
-        path: '/zip',
-        component: Layout,
-        redirect: '/zip/download',
-        alwaysShow: true,
-        name: 'Zip',
-        meta: {title: 'Zip', icon: 'zip'},
-        children: [
-            {
-                path: 'download',
-                component: () => import('@/views/zip/index'),
-                name: 'ExportZip',
-                meta: {title: 'Export Zip'}
-            }
-        ]
-    },
-
-    {
-        path: '/pdf',
-        component: Layout,
-        redirect: '/pdf/index',
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/pdf/index'),
-                name: 'PDF',
-                meta: {title: 'PDF', icon: 'pdf'}
-            }
-        ]
-    },
-    {
-        path: '/pdf/download',
-        component: () => import('@/views/pdf/download'),
-        hidden: true
-    },
-
-    {
-        path: '/theme',
-        component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/theme/index'),
-                name: 'Theme',
-                meta: {title: 'Theme', icon: 'theme'}
-            }
-        ]
-    },
-
-    {
-        path: '/clipboard',
-        component: Layout,
-        children: [
-            {
-                path: 'index',
-                component: () => import('@/views/clipboard/index'),
-                name: 'ClipboardDemo',
-                meta: {title: 'Clipboard', icon: 'clipboard'}
-            }
-        ]
-    },
-
-    {
-        path: 'external-link',
-        component: Layout,
-        children: [
-            {
-                path: 'https://github.com/PanJiaChen/vue-element-admin',
-                meta: {title: 'External Link', icon: 'link'}
-            }
-        ]
-    },
-
-    // 404 page must be placed at the end !!!
     {path: '*', redirect: '/404', hidden: true}
 ]
 
